d669126405395fe95f057b2b009bbe807246d775
etinolo <127261463+etinolo@users.noreply.github.com>
2023-03-08 23:38:55 +0100

    Initial commit

---
+++ Topics/The for-loop/Arithmetic average/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        double count = 0;
+        double sum = 0;
+        for (int i = a; i <= b; i++) {
+            if (i % 3 == 0) {
+                sum += i;
+                count++;
+            }
+        }
+        System.out.println(sum / count);
+    }
+}
